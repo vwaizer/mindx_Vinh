a4fa8124564ded2d0ad1f372414b789e78c9f9db
vwaizer <lightwing2208@gmail.com>
2023-07-10 20:52:05 +0700

    final

---
+++ tuần 6/Nguyễn Quang Vinh_tuần 6.js
@@ -0,0 +1,10 @@
+let y=20;
+this.document.write("<br> y=" + y);
+var x;
+this.document.write("<br> x=" + x);
+
+let o ={
+    name:"mindx",
+    addr:"abcd"
+}
+this.document.write("<br> o="+ JSON.stringify(o))
