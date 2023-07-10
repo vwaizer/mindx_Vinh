fb79cfb29d91f80211aba41c7fe70dd54785d6a9
vwaizer <lightwing2208@gmail.com>
2023-07-10 21:41:54 +0700

    1st

---
+++ tuần 6/QuangVinhWeek6.js
@@ -0,0 +1,56 @@
+//2
+var age = 18;
+var nameSchool;
+var studentScore=10;
+let radius=5;
+const PI=3.14;
+let studentName;
+//3
+age="MindX Dream";
+//4
+var stringNumber1="Điểm 10";
+var stringNumber2=" là điểm ";
+var stringNumber3="tuyệt đối";
+this.document.write(stringNumber1+stringNumber2+stringNumber3+"<br>");
+//5
+var result;
+result=radius*radius*PI;
+this.document.write("Diện tích hình tròn :"+result+"<br>");
+//6
+// tên của bạn 
+var nameOfFriend1="Minh";
+var nameOfFriend2="Hoàng";
+var nameOfFriend3="Tùng";
+var nameOfFriend4="Nhân";
+var nameOfFriend5="Nam";
+//tuổi của bạn
+var ageOfFriend1=18;
+var ageOfFriend2=19;
+var ageOfFriend3=20;
+var ageOfFriend4=21;
+var ageOfFriend5=22;
+//nối tên và tuổi
+this.document.write("Tên: "+nameOfFriend1+" Tuổi: "+ageOfFriend1+"<br>");
+this.document.write("Tên: "+nameOfFriend2+" Tuổi: "+ageOfFriend2+"<br>");
+this.document.write("Tên: "+nameOfFriend3+" Tuổi: "+ageOfFriend3+"<br>");
+this.document.write("Tên: "+nameOfFriend4+" Tuổi: "+ageOfFriend4+"<br>");
+this.document.write("Tên: "+nameOfFriend5+" Tuổi: "+ageOfFriend5+"<br>");
+//in chieu dai ten
+var char ="a";
+result= nameOfFriend1.length;
+this.document.write("chiều dài tên: "+result+"<br>");
+result= nameOfFriend2.length;
+this.document.write("chiều dài tên: "+result+"<br>");
+result= nameOfFriend3.length;
+this.document.write("chiều dài tên: "+result+"<br>");
+result= nameOfFriend4.length;
+this.document.write("chiều dài tên: "+result+"<br>");
+result= nameOfFriend5.length;
+this.document.write("chiều dài tên: "+result+"<br>");
+//in tuoi trung binh
+result=(ageOfFriend1+ ageOfFriend2+ ageOfFriend3+ageOfFriend4+ageOfFriend5)/5
+this.document.write("Tuổi trung bình : "+result+"<br>");
+//in ra màn hình tổng số tuổi
+result *=5;
+this.document.write("tổng số tuổi: "+result+"<br>");
+
