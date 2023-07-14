30242f5c87d7065ce16f1e23c430de5f3ee7f8d9
vwaizer <lightwing2208@gmail.com>
2023-07-14 21:56:07 +0700

    add code final

--- tuần 7/Week7.js
+++ tuần 7/Week7.js
@@ -1,9 +1,9 @@
 //1
 {
     let NOfQuestion1 = 3.15569;
-   
+
     if (typeof NOfQuestion1 == "number") {
-        if (NOfQuestion1-(Math.ceil(NOfQuestion1)) < 1) {
+        if (NOfQuestion1 - (Math.ceil(NOfQuestion1)) < 1) {
             console.log(NOfQuestion1 + " không là số nguyên");
         }
         else {
@@ -16,8 +16,8 @@
 }
 //2
 {
-    let NOfQuestion2 = 1;
-    if (NOfQuestion2 > 2) {
+    let NOfQuestion2 = 2;
+    if (NOfQuestion2 > 2 && typeof NOfQuestion2 == "number") {
         let check = true;
         for (let i = 2; i < NOfQuestion2; i++) {
 
@@ -27,31 +27,35 @@
             }
         }
         if (check) {
-            console.log(N + " là số nguyên tố ");
+            console.log(NOfQuestion2 + " là số nguyên tố ");
         }
         else {
-            console.log(N + " không là số nguyên tố ");
+            console.log(NOfQuestion2 + " không là số nguyên tố ");
         }
 
     }
-    else if (NOfQuestion2 == 2) {
+    else if (NOfQuestion2 == 2 && typeof NOfQuestion2 == "number") {
         console.log("2 là số nguyên tố ");
     }
     else { console.log(NOfQuestion2 + " không là số nguyên tố "); }
 }
 //3
 {
+    
     let number = 0;
-    if (number % 2) {
-        console.log(number + "là số lẻ");
+    if (typeof number == "number") {
+        if (number % 2) {
+            console.log(number + "là số lẻ");
+        }
+        else { console.log(number + " là số chẵn"); }
     }
-    else { console.log(number + " là số chẵn"); }
+    else {console.log("không phải là number");}
 }
 //4
 {
-    let input = 25;
+    let input = "12";
     let result = "0";
-    if (input <= 0) {
+    if (input <= 0 || typeof input != "number") {
         console.log("không có số chính phương");
     }
     else {
