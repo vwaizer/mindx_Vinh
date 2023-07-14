4d1166da7f853cf9d0f8f2e0cae70473960058f4
vwaizer <lightwing2208@gmail.com>
2023-07-14 22:31:54 +0700

    final

--- tuần 6/QuangVinhWeek6Home.js
+++ tuần 6/QuangVinhWeek6Home.js
@@ -1,17 +1,17 @@
-function Total2Number(){
-    var firstNumber=this.document.getElementById("firstNumber").valueAsNumber;
-    var secondNumber=this.document.getElementById("secondNumber").valueAsNumber;
+{
+    var firstNumber=6;
+    var secondNumber=5;
    var resultTotal=firstNumber+ secondNumber;
    document.getElementById("resultTotal2Number").innerHTML = resultTotal;
 }
-function Perimeter(){
-    const Pi=3.14;
-    var radius=this.document.getElementById("radius").valueAsNumber;
-    var resultPerimeter=radius*radius*Pi;
+{
+    const PI=3.14;
+    var radius=56;
+    var resultPerimeter=radius*radius*PI;
     document.getElementById("resultPerimeter").innerHTML = resultPerimeter;
 }
-function CheckOddOrEven(){
-    var checkNumber=this.document.getElementById("checkOddOrEven").valueAsNumber;
+{
+    var checkNumber=5;
     if(checkNumber%2){
         document.getElementById("resultCheckOddOrEven").innerHTML = "ODD";
     }
@@ -19,14 +19,14 @@ function CheckOddOrEven(){
         document.getElementById("resultCheckOddOrEven").innerHTML = "Even";
     }
 }
-function ConnectString(){
-    var firstString=this.document.getElementById("firstString").value;
-    var secondString=this.document.getElementById("secondString").value;
+{
+    var firstString="hahah";
+    var secondString="adada";
     document.getElementById("resultConnectString").innerHTML = firstString+secondString;
 }
-function FindWordInString(){
-    var string=this.document.getElementById("string").value;
-    var word=this.document.getElementById("word").value;
+{
+    var string="yeah yeah";
+    var word="h";
     if(string.includes(word)){
         document.getElementById("resultFindWordInString").innerHTML = "True";
     }
@@ -34,8 +34,8 @@ function FindWordInString(){
         document.getElementById("resultFindWordInString").innerHTML = "False";
     }
 }
-function CheckNullOfString(){
-    var stringLength=this.document.getElementById("stringLength").value;
+{
+    var stringLength="adadadsadsda";
     if(stringLength.length){
         document.getElementById("resultCheckNullOfString").innerHTML = "Not Null";
     }
@@ -44,8 +44,8 @@ function CheckNullOfString(){
         document.getElementById("resultCheckNullOfString").innerHTML = "Null";
     }
 }
-function CheckPositiveOrNegative(){
-    var checkNumber=this.document.getElementById("checkPositiveOrNegative").valueAsNumber;
+{
+    var checkNumber=58;
     if(checkNumber>0){
         document.getElementById("resultCheckPositiveOrNegative").innerHTML = "Positive";
     }
@@ -58,9 +58,9 @@ function CheckPositiveOrNegative(){
         }
     }
 }
-function Calculate(){
-    var number1=this.document.getElementById("number1").valueAsNumber;
-    var number2=this.document.getElementById("number2").valueAsNumber;
+{
+    var number1=26;
+    var number2=15;
     var resultADD=number1+number2;
     var resultMinus=number1-number2;
     var resultMul=number1*number2;
