535da7941cff03e88c443aef796ed9de6c948552
vwaizer <lightwing2208@gmail.com>
2023-07-14 22:07:23 +0700

    add code final

--- tuần 7/Week7.js
+++ tuần 7/Week7.js
@@ -1,43 +1,43 @@
 //1
 {
-    let NOfQuestion1 = 3.15569;
+    let nOfQuestion1 = 3.3;
 
-    if (typeof NOfQuestion1 == "number") {
-        if (NOfQuestion1 - (Math.ceil(NOfQuestion1)) < 1) {
-            console.log(NOfQuestion1 + " không là số nguyên");
+    if (typeof nOfQuestion1 == "number") {
+        if (nOfQuestion1 - (Math.floor(nOfQuestion1)) < 1) {
+            console.log(nOfQuestion1 + " không là số nguyên");
         }
         else {
-            console.log(NOfQuestion1 + " là số nguyên");
+            console.log(nOfQuestion1 + " là số nguyên");
         }
     }
     else {
-        console.log(NOfQuestion1 + " không là số nguyên");
+        console.log(nOfQuestion1 + " không là số nguyên");
     }
 }
 //2
 {
-    let NOfQuestion2 = 2;
-    if (NOfQuestion2 > 2 && typeof NOfQuestion2 == "number") {
+    let nOfQuestion2 = 2;
+    if (nOfQuestion2 > 2 && typeof nOfQuestion2 == "number") {
         let check = true;
-        for (let i = 2; i < NOfQuestion2; i++) {
+        for (let i = 2; i < nOfQuestion2; i++) {
 
-            if (NOfQuestion2 % i == 0) {
+            if (nOfQuestion2 % i == 0) {
                 check = false;
                 break;
             }
         }
         if (check) {
-            console.log(NOfQuestion2 + " là số nguyên tố ");
+            console.log(nOfQuestion2 + " là số nguyên tố ");
         }
         else {
-            console.log(NOfQuestion2 + " không là số nguyên tố ");
+            console.log(nOfQuestion2 + " không là số nguyên tố ");
         }
 
     }
-    else if (NOfQuestion2 == 2 && typeof NOfQuestion2 == "number") {
+    else if (nOfQuestion2 == 2 && typeof nOfQuestion2 == "number") {
         console.log("2 là số nguyên tố ");
     }
-    else { console.log(NOfQuestion2 + " không là số nguyên tố "); }
+    else { console.log(nOfQuestion2 + " không là số nguyên tố "); }
 }
 //3
 {
